685af735a535028a546f7b5feea158e2ba339bf0
geekerstar <247507792@qq.com>
2018-11-06 10:23:49 +0800

    up 75

---
+++ src/com/geekerstar/s75/Solution.java
@@ -0,0 +1,22 @@
+package com.geekerstar.s75;
+
+public class Solution {
+    public void sortColors(int[] nums) {
+        int zero = -1, one = 0, two = nums.length;
+        while (one < two) {
+            if (nums[one] == 0) {
+                swap(nums, ++zero, one++);
+            } else if (nums[one] == 2) {
+                swap(nums, --two, one);
+            } else {
+                ++one;
+            }
+        }
+    }
+
+    private void swap(int[] nums, int i, int j) {
+        int t = nums[i];
+        nums[i] = nums[j];
+        nums[j] = t;
+    }
+}
