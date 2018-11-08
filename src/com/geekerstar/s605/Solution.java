cee2f6a9ae995cf1685ca88487e06785207e19c2
geekerstar <247507792@qq.com>
2018-11-08 20:43:49 +0800

    up s605

---
+++ src/com/geekerstar/s605/Solution.java
@@ -0,0 +1,20 @@
+package com.geekerstar.s605;
+
+public class Solution {
+    public boolean canPlaceFlowers(int[] flowerbed, int n) {
+        int len = flowerbed.length;
+        int cnt = 0;
+        for (int i = 0; i < len && cnt < n; i++) {
+            if (flowerbed[i] == 1) {
+                continue;
+            }
+            int pre = i == 0 ? 0 : flowerbed[i - 1];
+            int next = i == len - 1 ? 0 : flowerbed[i + 1];
+            if (pre == 0 && next == 0) {
+                cnt++;
+                flowerbed[i] = 1;
+            }
+        }
+        return cnt >= n;
+    }
+}
