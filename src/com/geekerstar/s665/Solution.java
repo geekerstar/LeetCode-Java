212f6c42661a082f7aa70f4603256da274aa7e11
geekerstar <247507792@qq.com>
2018-11-08 21:12:53 +0800

    up 665

---
+++ src/com/geekerstar/s665/Solution.java
@@ -0,0 +1,19 @@
+package com.geekerstar.s665;
+
+public class Solution {
+    public boolean checkPossibility(int[] nums) {
+        int cnt = 0;
+        for (int i = 1; i < nums.length && cnt < 2; i++) {
+            if (nums[i] >= nums[i - 1]) {
+                continue;
+            }
+            cnt++;
+            if (i - 2 >= 0 && nums[i - 2] > nums[i]) {
+                nums[i] = nums[i - 1];
+            } else {
+                nums[i - 1] = nums[i];
+            }
+        }
+        return cnt <= 1;
+    }
+}
