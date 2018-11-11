ee70418ece6813aec6a36058fc9d2688bceba96f
geekerstar <247507792@qq.com>
2018-11-11 21:25:40 +0800

    up 278

---
+++ src/com/geekerstar/s278/Solution.java
@@ -0,0 +1,16 @@
+package com.geekerstar.s278;
+
+//public class Solution extends VersionControl {
+//    public int firstBadVersion(int n) {
+//        int l = 1, h = n;
+//        while (l < h) {
+//            int mid = l + (h - l) / 2;
+//            if (isBadVersion(mid)) {
+//                h = mid;
+//            } else {
+//                l = mid + 1;
+//            }
+//        }
+//        return l;
+//    }
+//}
