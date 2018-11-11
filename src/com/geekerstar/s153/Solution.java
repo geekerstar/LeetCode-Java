b5ec176d13ca10a2663fce8fd3394b5ec35a85ea
geekerstar <247507792@qq.com>
2018-11-11 21:38:04 +0800

    up 153

---
+++ src/com/geekerstar/s153/Solution.java
@@ -0,0 +1,16 @@
+package com.geekerstar.s153;
+
+public class Solution {
+    public int findMin(int[] nums) {
+        int l = 0, h = nums.length - 1;
+        while (l < h) {
+            int m = l + (h - l) / 2;
+            if (nums[m] <= nums[h]) {
+                h = m;
+            } else {
+                l = m + 1;
+            }
+        }
+        return nums[l];
+    }
+}
