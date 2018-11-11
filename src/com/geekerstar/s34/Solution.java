bdc9ef5fe43e7c42e334fa63168a126fca2da82d
geekerstar <247507792@qq.com>
2018-11-11 21:45:18 +0800

    up 34

---
+++ src/com/geekerstar/s34/Solution.java
@@ -0,0 +1,26 @@
+package com.geekerstar.s34;
+
+public class Solution {
+    public int[] searchRange(int[] nums, int target) {
+        int first = binarySearch(nums, target);
+        int last = binarySearch(nums, target + 1) - 1;
+        if (first == nums.length || nums[first] != target) {
+            return new int[]{-1, -1};
+        } else {
+            return new int[]{first, Math.max(first, last)};
+        }
+    }
+
+    private int binarySearch(int[] nums, int target) {
+        int l = 0, h = nums.length; // 注意 h 的初始值
+        while (l < h) {
+            int m = l + (h - l) / 2;
+            if (nums[m] >= target) {
+                h = m;
+            } else {
+                l = m + 1;
+            }
+        }
+        return l;
+    }
+}
