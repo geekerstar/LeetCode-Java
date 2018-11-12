f7ee473822fa3e06ebb81a6b088029b3bb604209
geekerstar <247507792@qq.com>
2018-11-12 21:39:02 +0800

    up 547

---
+++ src/com/geekerstar/s547/Solution.java
@@ -0,0 +1,27 @@
+package com.geekerstar.s547;
+
+public class Solution {
+    private int n;
+
+    public int findCircleNum(int[][] M) {
+        n = M.length;
+        int circleNum = 0;
+        boolean[] hasVisited = new boolean[n];
+        for (int i = 0; i < n; i++) {
+            if (!hasVisited[i]) {
+                dfs(M, i, hasVisited);
+                circleNum++;
+            }
+        }
+        return circleNum;
+    }
+
+    private void dfs(int[][] M, int i, boolean[] hasVisited) {
+        hasVisited[i] = true;
+        for (int k = 0; k < n; k++) {
+            if (M[i][k] == 1 && !hasVisited[k]) {
+                dfs(M, k, hasVisited);
+            }
+        }
+    }
+}
