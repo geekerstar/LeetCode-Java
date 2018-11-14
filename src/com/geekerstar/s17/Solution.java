5be8a2ebe1bebeba7760229f98b76e14ff7b9729
geekerstar <247507792@qq.com>
2018-11-14 19:52:52 +0800

    up 17

---
+++ src/com/geekerstar/s17/Solution.java
@@ -0,0 +1,31 @@
+package com.geekerstar.s17;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution {
+    private static final String[] KEYS = {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+
+    public List<String> letterCombinations(String digits) {
+        List<String> combinations = new ArrayList<>();
+        if (digits == null || digits.length() == 0) {
+            return combinations;
+        }
+        doCombination(new StringBuilder(), combinations, digits);
+        return combinations;
+    }
+
+    private void doCombination(StringBuilder prefix, List<String> combinations, final String digits) {
+        if (prefix.length() == digits.length()) {
+            combinations.add(prefix.toString());
+            return;
+        }
+        int curDigits = digits.charAt(prefix.length()) - '0';
+        String letters = KEYS[curDigits];
+        for (char c : letters.toCharArray()) {
+            prefix.append(c);                         // 添加
+            doCombination(prefix, combinations, digits);
+            prefix.deleteCharAt(prefix.length() - 1); // 删除
+        }
+    }
+}
