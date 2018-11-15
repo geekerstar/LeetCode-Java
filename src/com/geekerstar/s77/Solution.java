8d65474fbbdb8f4aaaa3b80a09e7de0cee51e09a
geekerstar <247507792@qq.com>
2018-11-15 19:31:04 +0800

    up 77

---
+++ src/com/geekerstar/s77/Solution.java
@@ -0,0 +1,25 @@
+package com.geekerstar.s77;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution {
+    public List<List<Integer>> combine(int n, int k) {
+        List<List<Integer>> combinations = new ArrayList<>();
+        List<Integer> combineList = new ArrayList<>();
+        backtracking(combineList, combinations, 1, k, n);
+        return combinations;
+    }
+
+    private void backtracking(List<Integer> combineList, List<List<Integer>> combinations, int start, int k, final int n) {
+        if (k == 0) {
+            combinations.add(new ArrayList<>(combineList));
+            return;
+        }
+        for (int i = start; i <= n - k + 1; i++) {  // 剪枝
+            combineList.add(i);
+            backtracking(combineList, combinations, i + 1, k - 1, n);
+            combineList.remove(combineList.size() - 1);
+        }
+    }
+}
