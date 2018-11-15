bd4a7810ab7e7218da3304c1344d2824c27e7322
geekerstar <247507792@qq.com>
2018-11-15 19:38:50 +0800

    up 39

---
+++ src/com/geekerstar/s39/Solution.java
@@ -0,0 +1,28 @@
+package com.geekerstar.s39;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution {
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>> combinations = new ArrayList<>();
+        backtracking(new ArrayList<>(), combinations, 0, target, candidates);
+        return combinations;
+    }
+
+    private void backtracking(List<Integer> tempCombination, List<List<Integer>> combinations,
+                              int start, int target, final int[] candidates) {
+
+        if (target == 0) {
+            combinations.add(new ArrayList<>(tempCombination));
+            return;
+        }
+        for (int i = start; i < candidates.length; i++) {
+            if (candidates[i] <= target) {
+                tempCombination.add(candidates[i]);
+                backtracking(tempCombination, combinations, i, target - candidates[i], candidates);
+                tempCombination.remove(tempCombination.size() - 1);
+            }
+        }
+    }
+}
