838747acaae731ac2d7a386af83d6eaed72738b3
geekerstar <247507792@qq.com>
2018-11-19 19:56:16 +0800

    up 131

---
+++ src/com/geekerstar/s131/Solution.java
@@ -0,0 +1,36 @@
+package com.geekerstar.s131;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution {
+    public List<List<String>> partition(String s) {
+        List<List<String>> partitions = new ArrayList<>();
+        List<String> tempPartition = new ArrayList<>();
+        doPartition(s, partitions, tempPartition);
+        return partitions;
+    }
+
+    private void doPartition(String s, List<List<String>> partitions, List<String> tempPartition) {
+        if (s.length() == 0) {
+            partitions.add(new ArrayList<>(tempPartition));
+            return;
+        }
+        for (int i = 0; i < s.length(); i++) {
+            if (isPalindrome(s, 0, i)) {
+                tempPartition.add(s.substring(0, i + 1));
+                doPartition(s.substring(i + 1), partitions, tempPartition);
+                tempPartition.remove(tempPartition.size() - 1);
+            }
+        }
+    }
+
+    private boolean isPalindrome(String s, int begin, int end) {
+        while (begin < end) {
+            if (s.charAt(begin++) != s.charAt(end--)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
