96ed374c21ff10fc3813df471cf3110ee3e5a8b5
geekerstar <247507792@qq.com>
2018-11-19 19:41:43 +0800

    up 78

---
+++ src/com/geekerstar/s78/Solution.java
@@ -0,0 +1,30 @@
+package com.geekerstar.s78;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution {
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> subsets = new ArrayList<>();
+        List<Integer> tempSubset = new ArrayList<>();
+        for (int size = 0; size <= nums.length; size++) {
+            // 不同的子集大小
+            backtracking(0, tempSubset, subsets, size, nums);
+        }
+        return subsets;
+    }
+
+    private void backtracking(int start, List<Integer> tempSubset, List<List<Integer>> subsets,
+                              final int size, final int[] nums) {
+
+        if (tempSubset.size() == size) {
+            subsets.add(new ArrayList<>(tempSubset));
+            return;
+        }
+        for (int i = start; i < nums.length; i++) {
+            tempSubset.add(nums[i]);
+            backtracking(i + 1, tempSubset, subsets, size, nums);
+            tempSubset.remove(tempSubset.size() - 1);
+        }
+    }
+}
