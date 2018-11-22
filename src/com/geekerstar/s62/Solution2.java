e476a1847511a19a9a930e21064267d60eb8eca4
geekerstar <247507792@qq.com>
2018-11-22 19:28:25 +0800

    up 62

---
+++ src/com/geekerstar/s62/Solution2.java
@@ -0,0 +1,13 @@
+package com.geekerstar.s62;
+
+public class Solution2 {
+    public int uniquePaths(int m, int n) {
+        int S = m + n - 2;  // 总共的移动次数
+        int D = m - 1;      // 向下的移动次数
+        long ret = 1;
+        for (int i = 1; i <= D; i++) {
+            ret = ret * (S - D + i) / i;
+        }
+        return (int) ret;
+    }
+}
