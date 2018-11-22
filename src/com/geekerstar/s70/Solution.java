f6663c585961257911cfec3292bdf7b1711e4d42
geekerstar <247507792@qq.com>
2018-11-22 18:54:31 +0800

    up 70

---
+++ src/com/geekerstar/s70/Solution.java
@@ -0,0 +1,16 @@
+package com.geekerstar.s70;
+
+public class Solution {
+    public int climbStairs(int n) {
+        if (n <= 2) {
+            return n;
+        }
+        int pre2 = 1, pre1 = 2;
+        for (int i = 2; i < n; i++) {
+            int cur = pre1 + pre2;
+            pre2 = pre1;
+            pre1 = cur;
+        }
+        return pre1;
+    }
+}
