2650a4c0c5388be3e2c5c7c0006e721d4b8454b1
geekerstar <247507792@qq.com>
2018-11-22 20:01:46 +0800

    up 413

---
+++ src/com/geekerstar/s413/Solution.java
@@ -0,0 +1,21 @@
+package com.geekerstar.s413;
+
+public class Solution {
+    public int numberOfArithmeticSlices(int[] A) {
+        if (A == null || A.length == 0) {
+            return 0;
+        }
+        int n = A.length;
+        int[] dp = new int[n];
+        for (int i = 2; i < n; i++) {
+            if (A[i] - A[i - 1] == A[i - 1] - A[i - 2]) {
+                dp[i] = dp[i - 1] + 1;
+            }
+        }
+        int total = 0;
+        for (int cnt : dp) {
+            total += cnt;
+        }
+        return total;
+    }
+}
