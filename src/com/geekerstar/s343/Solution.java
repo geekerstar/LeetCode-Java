8f5222118d9faead2163291bcd3ab9ac4783f001
geekerstar <247507792@qq.com>
2018-11-22 20:04:55 +0800

    up 343

---
+++ src/com/geekerstar/s343/Solution.java
@@ -0,0 +1,14 @@
+package com.geekerstar.s343;
+
+public class Solution {
+    public int integerBreak(int n) {
+        int[] dp = new int[n + 1];
+        dp[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            for (int j = 1; j <= i - 1; j++) {
+                dp[i] = Math.max(dp[i], Math.max(j * dp[i - j], j * (i - j)));
+            }
+        }
+        return dp[n];
+    }
+}
