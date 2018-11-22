b5c565f27be7d5b41856e49d048ceab0e5cd25e1
geekerstar <247507792@qq.com>
2018-11-22 19:21:25 +0800

    up 64

---
+++ src/com/geekerstar/s64/Solution.java
@@ -0,0 +1,22 @@
+package com.geekerstar.s64;
+
+public class Solution {
+    public int minPathSum(int[][] grid) {
+        if (grid.length == 0 || grid[0].length == 0) {
+            return 0;
+        }
+        int m = grid.length, n = grid[0].length;
+        int[] dp = new int[n];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == 0) {
+                    dp[j] = dp[j - 1];
+                } else {
+                    dp[j] = Math.min(dp[j - 1], dp[j]);
+                }
+                dp[j] += grid[i][j];
+            }
+        }
+        return dp[n - 1];
+    }
+}
