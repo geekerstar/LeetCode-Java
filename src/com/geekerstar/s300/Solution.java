eb4ba1a0dc2b52e21ff0b3e181f8349fd18dddb0
geekerstar <247507792@qq.com>
2018-11-23 22:19:38 +0800

    up 300

---
+++ src/com/geekerstar/s300/Solution.java
@@ -0,0 +1,20 @@
+package com.geekerstar.s300;
+
+import java.util.Arrays;
+
+public class Solution {
+    public int lengthOfLIS(int[] nums) {
+        int n = nums.length;
+        int[] dp = new int[n];
+        for (int i = 0; i < n; i++) {
+            int max = 1;
+            for (int j = 0; j < i; j++) {
+                if (nums[i] > nums[j]) {
+                    max = Math.max(max, dp[j] + 1);
+                }
+            }
+            dp[i] = max;
+        }
+        return Arrays.stream(dp).max().orElse(0);
+    }
+}
