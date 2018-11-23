0e5d179830ab892fd21dc367757567178933a6e3
geekerstar <247507792@qq.com>
2018-11-23 22:57:15 +0800

    up 646

---
+++ src/com/geekerstar/s646/Solution.java
@@ -0,0 +1,24 @@
+package com.geekerstar.s646;
+
+import java.util.Arrays;
+
+public class Solution {
+    public int findLongestChain(int[][] pairs) {
+        if (pairs == null || pairs.length == 0) {
+            return 0;
+        }
+        Arrays.sort(pairs, (a, b) -> (a[0] - b[0]));
+        int n = pairs.length;
+        int[] dp = new int[n];
+        Arrays.fill(dp, 1);
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                if (pairs[j][1] < pairs[i][0]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+        }
+        return Arrays.stream(dp).max().orElse(0);
+    }
+
+}
