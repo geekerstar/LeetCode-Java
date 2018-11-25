95d858fa4c3308e167cc8d25bf8b17aeef1b99ea
geekerstar <247507792@qq.com>
2018-11-25 20:17:47 +0800

    up 474

---
+++ src/com/geekerstar/s474/Solution.java
@@ -0,0 +1,27 @@
+package com.geekerstar.s474;
+
+public class Solution {
+    public int findMaxForm(String[] strs, int m, int n) {
+        if (strs == null || strs.length == 0) {
+            return 0;
+        }
+        int[][] dp = new int[m + 1][n + 1];
+        // 每个字符串只能用一次
+        for (String s : strs) {
+            int ones = 0, zeros = 0;
+            for (char c : s.toCharArray()) {
+                if (c == '0') {
+                    zeros++;
+                } else {
+                    ones++;
+                }
+            }
+            for (int i = m; i >= zeros; i--) {
+                for (int j = n; j >= ones; j--) {
+                    dp[i][j] = Math.max(dp[i][j], dp[i - zeros][j - ones] + 1);
+                }
+            }
+        }
+        return dp[m][n];
+    }
+}
