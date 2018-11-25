52c13aa43362de738570dfdc39cff4935da42009
geekerstar <247507792@qq.com>
2018-11-25 20:53:40 +0800

    up 583

---
+++ src/com/geekerstar/s583/Solution.java
@@ -0,0 +1,18 @@
+package com.geekerstar.s583;
+
+public class Solution {
+    public int minDistance(String word1, String word2) {
+        int m = word1.length(), n = word2.length();
+        int[][] dp = new int[m + 1][n + 1];
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (word1.charAt(i - 1) == word2.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                } else {
+                    dp[i][j] = Math.max(dp[i][j - 1], dp[i - 1][j]);
+                }
+            }
+        }
+        return m + n - 2 * dp[m][n];
+    }
+}
