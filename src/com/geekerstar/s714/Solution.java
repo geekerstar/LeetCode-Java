a6318627e38723dd823d8e7317cebbb92e14c2c2
geekerstar <247507792@qq.com>
2018-11-25 20:45:28 +0800

    up 714

---
+++ src/com/geekerstar/s714/Solution.java
@@ -0,0 +1,20 @@
+package com.geekerstar.s714;
+
+public class Solution {
+    public int maxProfit(int[] prices, int fee) {
+        int N = prices.length;
+        int[] buy = new int[N];
+        int[] s1 = new int[N];
+        int[] sell = new int[N];
+        int[] s2 = new int[N];
+        s1[0] = buy[0] = -prices[0];
+        sell[0] = s2[0] = 0;
+        for (int i = 1; i < N; i++) {
+            buy[i] = Math.max(sell[i - 1], s2[i - 1]) - prices[i];
+            s1[i] = Math.max(buy[i - 1], s1[i - 1]);
+            sell[i] = Math.max(buy[i - 1], s1[i - 1]) - fee + prices[i];
+            s2[i] = Math.max(s2[i - 1], sell[i - 1]);
+        }
+        return Math.max(sell[N - 1], s2[N - 1]);
+    }
+}
