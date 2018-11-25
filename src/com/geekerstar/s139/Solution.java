c829f7ec376800e83680bd189524b43e5c64d895
geekerstar <247507792@qq.com>
2018-11-25 20:14:43 +0800

    up 139

---
+++ src/com/geekerstar/s139/Solution.java
@@ -0,0 +1,21 @@
+package com.geekerstar.s139;
+
+import java.util.List;
+
+public class Solution {
+    public boolean wordBreak(String s, List<String> wordDict) {
+        int n = s.length();
+        boolean[] dp = new boolean[n + 1];
+        dp[0] = true;
+        for (int i = 1; i <= n; i++) {
+            // 完全一个物品可以使用多次
+            for (String word : wordDict) {
+                int len = word.length();
+                if (len <= i && word.equals(s.substring(i - len, i))) {
+                    dp[i] = dp[i] || dp[i - len];
+                }
+            }
+        }
+        return dp[n];
+    }
+}
