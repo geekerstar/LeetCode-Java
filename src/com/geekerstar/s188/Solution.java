e7e73221c4c47ab3a3ea0ac569b298042cf7485b
geekerstar <247507792@qq.com>
2018-11-25 20:49:54 +0800

    up 188

---
+++ src/com/geekerstar/s188/Solution.java
@@ -0,0 +1,26 @@
+package com.geekerstar.s188;
+
+public class Solution {
+    public int maxProfit(int k, int[] prices) {
+        int n = prices.length;
+        // 这种情况下该问题退化为普通的股票交易问题
+        if (k >= n / 2) {
+            int maxProfit = 0;
+            for (int i = 1; i < n; i++) {
+                if (prices[i] > prices[i - 1]) {
+                    maxProfit += prices[i] - prices[i - 1];
+                }
+            }
+            return maxProfit;
+        }
+        int[][] maxProfit = new int[k + 1][n];
+        for (int i = 1; i <= k; i++) {
+            int localMax = maxProfit[i - 1][0] - prices[0];
+            for (int j = 1; j < n; j++) {
+                maxProfit[i][j] = Math.max(maxProfit[i][j - 1], prices[j] + localMax);
+                localMax = Math.max(localMax, maxProfit[i - 1][j] - prices[j]);
+            }
+        }
+        return maxProfit[k][n - 1];
+    }
+}
