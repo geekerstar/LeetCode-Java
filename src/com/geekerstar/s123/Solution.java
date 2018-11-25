505b12427d9f8babc4481a036fe48d5f6f3de429
geekerstar <247507792@qq.com>
2018-11-25 20:47:29 +0800

    up 123

---
+++ src/com/geekerstar/s123/Solution.java
@@ -0,0 +1,23 @@
+package com.geekerstar.s123;
+
+public class Solution {
+    public int maxProfit(int[] prices) {
+        int firstBuy = Integer.MIN_VALUE, firstSell = 0;
+        int secondBuy = Integer.MIN_VALUE, secondSell = 0;
+        for (int curPrice : prices) {
+            if (firstBuy < -curPrice) {
+                firstBuy = -curPrice;
+            }
+            if (firstSell < firstBuy + curPrice) {
+                firstSell = firstBuy + curPrice;
+            }
+            if (secondBuy < firstSell - curPrice) {
+                secondBuy = firstSell - curPrice;
+            }
+            if (secondSell < secondBuy + curPrice) {
+                secondSell = secondBuy + curPrice;
+            }
+        }
+        return secondSell;
+    }
+}
